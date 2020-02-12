66475f4e668181769ceddee1de3b0e783969dbfa
rgpacas8 <43006093+rgpacas8@users.noreply.github.com>
2020-02-11 20:28:16 -0600

    Add files via upload

--- rtos.c
+++ rtos.c
@@ -48,7 +48,6 @@ typedef enum
 {
 	S_READY = 0, S_RUNNING, S_WAITING, S_SUSPENDED
 } task_state_e;
-
 typedef enum
 {
 	kFromISR = 0, kFromNormalExec
@@ -76,8 +75,7 @@ struct
 	rtos_task_handle_t next_task;
 	rtos_tcb_t tasks[RTOS_MAX_NUMBER_OF_TASKS + 1];
 	rtos_tick_t global_tick;
-} task_list =
-{ 0 };
+} task_list ={ 0 };
 
 /**********************************************************************************/
 // Local methods prototypes
@@ -98,69 +96,64 @@ void rtos_start_scheduler(void)
 #ifdef RTOS_ENABLE_IS_ALIVE
 	init_is_alive();
 #endif
-	SysTick->CTRL = SysTick_CTRL_CLKSOURCE_Msk | SysTick_CTRL_TICKINT_Msk
-	        | SysTick_CTRL_ENABLE_Msk;
+	SysTick->CTRL = SysTick_CTRL_CLKSOURCE_Msk | SysTick_CTRL_TICKINT_Msk | SysTick_CTRL_ENABLE_Msk;
 	reload_systick();
-	for (;;)
-		;
+	for (;;);
 }
 
-rtos_task_handle_t rtos_create_task(void (*task_body)(), uint8_t priority,
-		rtos_autostart_e autostart)
+rtos_task_handle_t rtos_create_task(void (*task_body)(), uint8_t priority,rtos_autostart_e autostart)
 {
+
 	rtos_tcb_t new_task;
 
-	if (task_list.nTasks < RTOS_MAX_NUMBER_OF_TASKS)
-	{
-		uint8_t priority;
-		task_state_e state;
-		uint32_t *sp;
-		void (*task_body)();
-		rtos_tick_t local_tick;
-		uint32_t reserved[10];
-		uint32_t stack[RTOS_STACK_SIZE];
-
-		if (autostart == kAutoStart)
+	if(RTOS_MAX_NUMBER_OF_TASKS>task_list.nTasks){
+		if(autostart == kAutoStart)
 		{
-//			new_task.priority = priority;
+			new_task.priority =priority;
 			new_task.state = S_READY;
-//			new_task.sp =
-//			new_task.task_body = task_body;
-//			new_task.local_tick =
-//			new_task.reserved =
-//			new_task.stack =
+			new_task.task_body = task_body;
+			task_list.tasks[task.list.nTasks]= new_task;
+
 		}
+		new_task.state = S_SUSPENDED;
+		task_list.tasks[task_list.nTasks].sp = &(task_list.tasks[task_list.nTasks].stack[RTOS_STACK_SIZE - 1 - STACK_FRAME_SIZE]);//apunta una direccion encima del stack frame
 
+		new_task.stack[STACK_PSR_OFFSET] = STACK_PSR_DEFAULT;
+		new_task.local_tick=0;//cada cuanto de realiza la tarea
 
+		//////////////////////////////////////////////////////////////////////////////////7
+		task_list.tasks[task_list.nTasks].priority = priority;
+		task_list.tasks[task_list.nTasks].local_tick = 0;
+		task_list.tasks[task_list.nTasks].task_body = task_body;
+		task_list.tasks[task_list.nTasks].sp =
+				&(task_list.tasks[task_list.nTasks].stack[RTOS_STACK_SIZE - 1
+						- STACK_FRAME_SIZE]);
+		task_list.tasks[task_list.nTasks].state =
+				kStartSuspended == autostart ? S_SUSPENDED : S_READY;
 
-	}
-
+		task_list.nTasks++;
+	};
 
 }
 
 rtos_tick_t rtos_get_clock(void)
 {
-	rtos_tick_t global_tick = task_list.global_tick;
-	return global_tick;
+	return task_list.global_tick;
 }
 
 void rtos_delay(rtos_tick_t ticks)
 {
-	task_list.tasks[task_list.current_task].state = S_WAITING;		/* La actual tarea en ESPERA */
-	task_list.tasks[task_list.current_task].local_tick = ticks; /* Asigna tick al reloj local de la tarea */
-	//dispatcher();	// kFromISR, kFromNormalExec 				/* Llama dispatcher (desde la tarea) */
+
 }
 
 void rtos_suspend_task(void)
 {
-	task_list.tasks[task_list.current_task].state = S_SUSPENDED;	/* La actual tarea en SUSPENDIDA */
-	//dispatcher();	// kFromISR, kFromNormalExec 			/* Llama dispatcher (desde la tarea) */
+
 }
 
 void rtos_activate_task(rtos_task_handle_t task)
 {
-	task_list.tasks[task_list.current_task].state = S_READY;		/* La actual tarea en LISTO */
-	//dispatcher();	// kFromISR, kFromNormalExec 			/* Llama dispatcher (desde la tarea) */
+
 }
 
 /**********************************************************************************/
@@ -169,8 +162,7 @@ void rtos_activate_task(rtos_task_handle_t task)
 
 static void reload_systick(void)
 {
-	SysTick->LOAD = USEC_TO_COUNT(RTOS_TIC_PERIOD_IN_US,
-	        CLOCK_GetCoreSysClkFreq());
+	SysTick->LOAD = USEC_TO_COUNT(RTOS_TIC_PERIOD_IN_US, CLOCK_GetCoreSysClkFreq());
 	SysTick->VAL = 0;
 }
 
@@ -186,19 +178,7 @@ FORCE_INLINE static void context_switch(task_switch_type_e type)
 
 static void activate_waiting_tasks()
 {
-	uint8_t i = 0;
-	for (i = 0; i < task_list.nTasks; i++) 		  /* Recorremos la lista total de tareas */
-	{
-		if (task_list.tasks[i].state = S_WAITING) /* ¿Tarea en estado de ESPERA? */
-		{
-			task_list.tasks[i].local_tick = task_list.tasks[i].local_tick - 1; 	 /* Disminuye en 1 el reloj local de la tarea */
 
-			if (task_list.tasks[i].local_tick == 0)/* ¿Reloj local de tarea es igual a 0? */
-			{
-				task_list.tasks[i].state = S_READY;/* Ponemos la tarea en estado LISTO */
-			}
-		}
-	}
 }
 
 /**********************************************************************************/
@@ -228,7 +208,11 @@ void SysTick_Handler(void)
 
 void PendSV_Handler(void)
 {
-
+	register int32_t r0 asm("r0");
+	(void) r0;
+	SCB->ICSR |= SCB_ICSR_PENDSVCLR_Msk;
+	r0 = (int32_t) task_list.tasks[task_list.current_task].sp;
+	asm("mov r7,r0");
 }
 
 /**********************************************************************************/
